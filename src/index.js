e45870425dbefd5a697a1317ce3a94b7c6aa8fce
Shivu2602 <96486366+Shivu2602@users.noreply.github.com>
2022-05-15 19:14:53 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// const root = ReactDOM.createRoot(document.getElementsByClassName('root')[0])
+
+root.render(<h1>Our first react app🔥</h1>)    //render means- display,put
